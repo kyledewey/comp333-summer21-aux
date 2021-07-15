43c892d63441ad84bc5c78331258026f5d3e0f95
Kyle Dewey <kyle.dewey@csun.edu>
2021-07-15 10:27:39 -0700

    took out code duplication to functions

--- week_1/virtual_dispatch_motivation/Temp.java
+++ week_1/virtual_dispatch_motivation/Temp.java
@@ -3,19 +3,37 @@ public class Temp {
     public static String getDestinationFile(String[] args) {
         // do something
     }
-    public static void main(String[] args) {
-        boolean userWantsConsole = doesUserWantConsole(args);
 
-        int result = doComputation();
-
+    public static void writeThing(boolean userWantsConsole,
+                                  String destinationFile,
+                                  int thing) {
         if (userWantsConsole) {
-            System.out.println(result);
+            System.out.println(thing);
         } else {
-            String destinationFile = getDestinationFile(args);
             File file = new File(destinationFile);
             FileOutputStream stream = new FileOutputStream(file);
-            stream.writeln(result);
+            stream.writeln(thing);
             stream.close();
         }
+    }
+    
+    public static int doComputation(boolean userWantsConsole,
+                                    String destinationFile) {
+        // do part #1 / 10 of computation
+
+        // print result of #1 / 10
+        int tempResult = ...;
+        writeThing(userWantsConsole, destinationFile, tempResult);
+        
+        // continue to do rest of computation
+    }
+    
+    public static void main(String[] args) {
+        boolean userWantsConsole = doesUserWantConsole(args);
+        String destinationFile = getDestinationFile(args);
+
+        int result = doComputation(userWantsConsole, destinationFile);
+
+        writeThing(userWantsConsole, destinationFile, tempResult);
     } // main
 } // Temp
